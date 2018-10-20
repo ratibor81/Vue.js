17c495d516e4442411a4084efcd781ce9ee05a83
unknown <smrserg2017@gmail.com>
2018-10-20 23:32:15 +0300

    sdfdsf

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -4,7 +4,7 @@
     infinite-scroll-disabled="busy"
     infinite-scroll-distance="10"
   >
-    <h2 v-if="movies.length === 0">Nothing found on your requset :(</h2>
+    <h2 v-if="movies.length===0">Nothing found on your requset :(</h2>
     <movie-list :movies="movies" />
   </div>
 </template>
@@ -13,6 +13,7 @@
 <script>
 import Vue from 'vue';
 import infiniteScroll from 'vue-infinite-scroll';
+import { searchMovie } from '../api/movies-api';
 import MovieList from '../MovieList.vue';
 
 Vue.use(infiniteScroll);
@@ -23,21 +24,39 @@ export default {
   components: {
     MovieList,
   },
+  data() {
+    return {
+      movies: [],
+    };
+  },
 
   computed: {
-    movies() {
-      return this.$store.getters.search;
-    },
+    // movies() {
+    //   return this.$store.getters.search;
+    // },
     title() {
       return this.$store.getters.title;
     },
   },
-
+  created() {
+    searchMovie(this.title).then((movies) => {
+      this.movies = movies;
+    });
+  },
+  updated() {
+    if (!this.title) return;
+    searchMovie(this.title).then((movies) => {
+      this.movies = movies;
+    });
+  },
   methods: {
     loadMore() {
       this.busy = false;
-      if (!this.title) return;
-      this.$store.dispatch('GET_MORE', this.title, pageNum);
+      // if (!this.title) return;
+      searchMovie(this.title, pageNum).then((movies) => {
+        this.movies = this.movies.concat(movies);
+      });
+      // this.$store.dispatch('GET_MORE', this.title, pageNum);
       this.busy = true;
       pageNum += 1;
     },
