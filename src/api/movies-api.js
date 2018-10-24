c366dd524995085bcf43bb04f01ac2dbd216a5b0
unknown <smrserg2017@gmail.com>
2018-10-24 07:43:12 +0300

    Add code-splitting and async/await

--- src/api/movies-api.js
+++ src/api/movies-api.js
@@ -3,27 +3,32 @@ import axios from 'axios';
 const API_KEY = '532f680f186ee3009db06b2e2efe9aab';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-export const fetchMovies = (page) => {
+export const fetchMovies = async (page) => {
   const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-EN&page=${page}`;
-  return axios.get(url).then(response => response.data.results);
+  const response = await axios.get(url);
+  return response.data.results;
 };
 
-export const searchMovie = (title, page) => {
+export const searchMovie = async (title, page) => {
   const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-EN&query=${title}&page=${page}`;
-  return axios.get(url).then(response => response.data.results);
+  const response = await axios.get(url);
+  return response.data.results;
 };
 
-export const searchById = (id) => {
+export const searchById = async (id) => {
   const url = `${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=en-US`;
-  return axios.get(url).then(response => response.data);
+  const response = await axios.get(url);
+  return response.data;
 };
 
-export const getMoviesByGenreId = (genreId, pageNum) => {
+export const getMoviesByGenreId = async (genreId, pageNum) => {
   const url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-EN&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNum}&with_genres=${genreId}`;
-  return axios.get(url).then(response => response.data.results);
+  const response = await axios.get(url);
+  return response.data.results;
 };
 
-export const getVideos = (id) => {
+export const getVideos = async (id) => {
   const url = `${BASE_URL}/movie/${id}/videos?api_key=${API_KEY}&language=en-EN`;
-  return axios.get(url).then(response => response.data.results[0]);
+  const response = await axios.get(url);
+  return response.data.results[0];
 };
